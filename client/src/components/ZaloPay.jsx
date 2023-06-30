c4de27830a09ea08b08f07f62e00b02e08341a14
dmanh <dmanh6302@gmail.com>
2023-06-30 21:51:49 +0700

    fix api

--- client/src/components/ZaloPay.jsx
+++ client/src/components/ZaloPay.jsx
@@ -1,5 +1,5 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, useToast } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import Helmet from 'react-helmet'
 import { Button } from '@chakra-ui/react'
 import axios from 'axios'
@@ -7,6 +7,11 @@ import axios from 'axios'
 const ZaloPay = ({ amount }) => {
 
   const userToken = localStorage.getItem('userToken');
+  const [paymentMethod, setPaymentMethod] = useState('ATM');
+  const toast = useToast();
+  const handleRadioChange = (event) => {
+    setPaymentMethod(event.target.value);
+  };
 
   const handleClick = async () => {
     try {
@@ -16,14 +21,25 @@ const ZaloPay = ({ amount }) => {
         },
       }
       const body = {
-        amount
+        amount: parseFloat(amount),
+        paymentMethod
       }
-
-      const { data } = await axios.post('/api/zalopay/create', body, config)
+      console.log(body)
+      const response = await axios.post('/api/zalopay/create', body, config)
+      console.log(response)
+      if (response.status === 200) {
+        window.open(response.data.order_url, '_blank');
+      }
       // Open a new tab with the order URL
-      window.open(data.order_url, '_blank');
     } catch (error) {
       console.log(error)
+      toast({
+        title: 'Something wrong',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-right',
+      });
     } finally {
       console.log("end of request")
     }
@@ -39,13 +55,13 @@ const ZaloPay = ({ amount }) => {
       <Box >
         <p>Vui lòng chọn hình thức thanh toán:</p>
         <div className="mb-1">
-          <label><input type="radio" name="iCheck" className="iradio_flat-blue" /> Ví <img src="/images/logo-zalopay.svg" alt="" style={{ display: "inline" }} /></label>
+          <label><input value={'zalopayapp'} onChange={handleRadioChange} type="radio" name="iCheck" className="iradio_flat-blue" /> Ví <img src="/images/logo-zalopay.svg" alt="" style={{ display: "inline" }} /></label>
         </div>
         <div className="mb-1">
-          <label><input type="radio" name="iCheck" className="iradio_flat-blue" /> Visa, Mastercard, JCB <span className="txtGray">(qua cổng ZaloPay)</span></label>
+          <label><input value={'CC'} onChange={handleRadioChange} type="radio" name="iCheck" className="iradio_flat-blue" /> Visa, Mastercard, JCB <span className="txtGray">(qua cổng ZaloPay)</span></label>
         </div>
         <div className="mb-1">
-          <label><input type="radio" name="iCheck" className="iradio_flat-blue" defaultChecked /> Thẻ ATM <span className="txtGray">(qua cổng ZaloPay)</span></label>
+          <label><input value={'ATM'} onChange={handleRadioChange} type="radio" name="iCheck" className="iradio_flat-blue" defaultChecked /> Thẻ ATM <span className="txtGray">(qua cổng ZaloPay)</span></label>
         </div>
         <Button colorScheme='blue' mb={3} onClick={handleClick}>Tiếp tục với ZaloPay</Button>
       </Box>
--- server/controllers/ZaloPayController.js
+++ server/controllers/ZaloPayController.js
@@ -1,35 +1,48 @@
-const axios = require('axios')
-const { getAppTime } = require('../helpers/ZalopayHelper')
+const axios = require('axios');
+const { getAppTime } = require('../helpers/ZalopayHelper');
 const ZalopayTransaction = require('../models/ZalopayTransactionModel');
 const CryptoJS = require('crypto-js');
 
 class ZaloPayController {
-
-  create = async (req, res) => {
+  async create(req, res) {
+    let transaction;
     try {
-      const transaction = await ZalopayTransaction.create({
-        amount: req.body.amount,
+      const { paymentMethod, amount } = req.body;
+      let embed_data = null;
+      let bank_code = null;
+
+      switch (paymentMethod) {
+        case 'CC':
+          bank_code = 'CC';
+          embed_data = JSON.stringify({});
+          break;
+        case 'ATM':
+          bank_code = '';
+          embed_data = JSON.stringify({ "bankgroup": 'ATM' });
+          break;
+        case 'zalopayapp':
+          bank_code = 'zalopayapp';
+          embed_data = JSON.stringify({});
+          break;
+        default:
+          throw new Error('Invalid payment method');
+      }
+
+      transaction = await ZalopayTransaction.create({
+        amount: parseFloat(amount),
         user_id: req.user._id,
       });
 
-      // Construct the body for ZaloPay request
       const currentDate = getAppTime();
-      const amount = transaction.amount;
-      const user_id = transaction.user_id.toString();
+      const user_id = req.user._id.toString();
       const app_id = parseInt(process.env.ZALOPAY_APP_ID);
       const app_user = user_id;
       const app_time = Date.now();
       const app_trans_id = `${currentDate}_${transaction._id}`;
       const item = JSON.stringify([]);
       const description = `Zalopay - Nạp tiền vào tài khoản - Giao dịch #${transaction._id}`;
-      const embed_data = JSON.stringify({});
-      const bank_code = 'zalopayapp';
-
-      // Construct the hmac_input string and key
       const key1 = process.env.ZALOPAY_APP_KEY;
       const hmac_input = `${app_id}|${app_trans_id}|${app_user}|${amount}|${app_time}|${embed_data}|${item}`;
-
-      // Generate HMAC
       const hmac = CryptoJS.HmacSHA256(hmac_input, key1);
       const mac = hmac.toString(CryptoJS.enc.Hex);
 
@@ -50,28 +63,20 @@ class ZaloPayController {
 
       res.json(data);
     } catch (error) {
-      // Handle error and update transaction status
       if (error.response && error.response.data) {
-        // ZaloPay API error
         console.error('ZaloPay API Error:', error.response.data);
-        // Handle the specific error and send an appropriate response to the client
         res.status(error.response.status).json({ error: 'ZaloPay API Error' });
       } else {
-        // Other errors
         console.error('Internal Server Error:', error);
         res.status(500).json({ error: 'Internal Server Error' });
       }
 
-      // Update transaction status to 'Cancel'
       if (transaction) {
         transaction.status = 'Cancel';
         await transaction.save();
       }
     }
   }
-
-
-
 }
 
 module.exports = new ZaloPayController();
